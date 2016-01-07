96943ecfb6f299352aabca3d04cd1490776bcc01
Tim Cera <tim@cerazone.net>
2016-01-06 21:03:34 -0500

    Why me?

--- setup.py
+++ setup.py
@@ -25,6 +25,11 @@ install_requires = [
     'tstoolbox >= 0.9.7',
 ]
 
+libraries = []
+if sys.platform.startswith('win'):
+    libraries = ['quadmath']
+    libraries = []
+
 wdm_support = Extension('wdm', [
           'wdm_support/wdm.pyf',
           'wdm_support/DTTM90.f',
