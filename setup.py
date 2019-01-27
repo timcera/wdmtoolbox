7665be7c84c7f44e7101d866c389930825bae937
Tim Cera <tim@cerazone.net>
2019-01-27 00:28:46 -0500

    BUG: tstoolbox bug fixed, but only able to test on Travis.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    'tstoolbox >= 30.75.35.23',
+    'tstoolbox >= 30.76.35.23',
     'filelock',
 ]
 
