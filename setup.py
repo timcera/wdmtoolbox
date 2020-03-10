3cb024d0efec5ef9aa55aa9e1dbbc812f674dc17
Tim Cera <tim@cerazone.net>
2020-03-10 07:41:36 -0400

    CI: Normalize to tstoolbox.

--- setup.py
+++ setup.py
@@ -24,22 +24,7 @@ version = open("VERSION").readline().strip()
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist")
-
-    # The following block of code is to set the timestamp on files to
-    # 'now', otherwise ChromeOS/google drive sets to 1970-01-01 and then
-    # no one can install it because zip doesn't support dates before
-    # 1980.
-    os.chdir("dist")
-    os.system("tar xvzf {pkg_name}-{version}.tar.gz".format(**locals()))
-    os.system("find {pkg_name}-{version}* -exec touch {{}} \\;".format(**locals()))
-    os.system(
-        "tar czf {pkg_name}-{version}.tar.gz {pkg_name}-{version}".format(**locals())
-    )
-    shutil.rmtree("{pkg_name}-{version}".format(**locals()))
-    os.chdir("..")
-
     os.system("twine upload dist/{pkg_name}-{version}.tar.gz".format(**locals()))
-    os.system("twine upload dist/{pkg_name}-{version}*.whl".format(**locals()))
     sys.exit()
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -103,8 +88,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
