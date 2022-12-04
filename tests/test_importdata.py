d2211e6de9c3145ac48f1a8040f236d7954e20f4
Tim Cera <tim@cerazone.net>
2022-12-04 05:59:01 -0500

    test(test_importdata.py): made test work with different wrapped lines

--- tests/test_importdata.py
+++ tests/test_importdata.py
@@ -91,7 +91,9 @@ class TestDescribe(TestCase):
             101,
             input_ts=os.path.join(self.test_dir, "nwisiv_02246000.csv"),
         )
-        with self.assertRaisesRegex(WDMError, "WDM error: data set number out of"):
+        with self.assertRaisesRegex(
+            WDMError, "(?s)WDM error: data set number.* out.* valid"
+        ):
             ret1 = wdmtoolbox.extract(self.wdmname, 0)
 
     def test_out_of_bounds_dsn(self):
@@ -102,7 +104,9 @@ class TestDescribe(TestCase):
             101,
             input_ts=os.path.join(self.test_dir, "nwisiv_02246000.csv"),
         )
-        with self.assertRaisesRegex(WDMError, "WDM error: data set number out of"):
+        with self.assertRaisesRegex(
+            WDMError, "(?s)WDM error: data set number.* out.* valid"
+        ):
             ret1 = wdmtoolbox.extract(self.wdmname, 32001)
 
     def test_dsn_not_in_wdm(self):
