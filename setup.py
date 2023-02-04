2b51a4d0ae583db120ead27299d17c6b6666bff1
Tim Cera <tim@cerazone.net>
2023-02-04 15:00:13 -0500

    build: small changes to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-import os
 import shlex
 import subprocess
 import sys
 
 import numpy as np
-from numpy.distutils.core import Extension, setup
+from numpy.distutils.core import setup
+from numpy.distutils.extension import Extension
 
 pkg_name = "wdmtoolbox"
 
-version = open("VERSION").readline().strip()
+version = open("VERSION", encoding="ascii").readline().strip()
 
 if sys.argv[-1] == "publish":
     subprocess.run(shlex.split("cleanpy ."), check=True)
     subprocess.run(shlex.split("python setup.py sdist"), check=True)
     subprocess.run(
-        shlex.split(f"twine upload dist/{pkg_name}-{version}.tar.gz"), check=True
+        shlex.split(f"twine upload --skip-existing dist/{pkg_name}-{version}.tar.gz"),
+        check=True,
     )
     subprocess.run(
-        shlex.split(f"twine upload dist/{pkg_name}-{version}*.whl"), check=True
+        shlex.split(f"twine upload --skip-existing dist/{pkg_name}-{version}*.whl"),
+        check=True,
     )
     sys.exit()
 
