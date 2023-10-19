31edade7443867f6c936755c3d2f7caa9b72cf66
Tim Cera <tim@cerazone.net>
2023-10-19 19:21:29 -0400

    ci: hopeful improvements to github workflows

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
+import os
 import shlex
+import shutil
 import subprocess
 import sys
-
-from setuptools import setup
 
 pkg_name = "wdmtoolbox"
 
@@ -10,11 +10,12 @@ version = open("VERSION", encoding="ascii").readline().strip()
 
 if sys.argv[-1] == "publish":
     subprocess.run(shlex.split("cleanpy ."), check=True)
+    if os.path.exists("dist"):
+        shutil.rmtree("dist")
+    if os.path.exists("build"):
+        shutil.rmtree("build")
     subprocess.run(shlex.split("python -m build"), check=True)
     subprocess.run(
         shlex.split(f"twine upload --skip-existing dist/{pkg_name}-{version}.tar.gz"),
         check=True,
     )
-    sys.exit()
-
-setup()
