a2f9463a19ab22283ef213849407af6c6bd0a36f
Tim Cera <tim@cerazone.net>
2024-03-30 08:58:23 -0400

    build: move setup.py to publish.py

---
+++ publish.py
@@ -0,0 +1,28 @@
+"""Publish package to PyPI."""
+
+import shlex
+import subprocess
+
+PKG_NAME = "wdmtoolbox"
+
+with open("VERSION", encoding="ascii") as version_file:
+    version = version_file.readline().strip()
+
+subprocess.run(shlex.split("pyclean ."), check=True)
+
+subprocess.run(shlex.split("python3 -m build --sdist"), check=True)
+subprocess.run(shlex.split("python3 -m build --wheel"), check=True)
+sdist = f"dist/{PKG_NAME}-{version}.tar.gz"
+wheel = f"dist/{PKG_NAME}-{version}*.whl"
+
+# leave off wheel for now since it only works for linux_x86_64 tag instead of
+# the manylinux tag.
+for file in [sdist]:
+    subprocess.run(
+        shlex.split(f"twine check {file}"),
+        check=True,
+    )
+    subprocess.run(
+        shlex.split(f"twine upload --skip-existing {file}"),
+        check=True,
+    )
