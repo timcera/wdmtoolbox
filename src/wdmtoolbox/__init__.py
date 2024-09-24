e1d6050de439a8715cefd07e22f534d6af53d235
Tim Cera <tim@cerazone.net>
2024-09-24 18:41:45 -0400

    ci: maybe fix windows build on github

--- src/wdmtoolbox/__init__.py
+++ src/wdmtoolbox/__init__.py
@@ -1,27 +1,11 @@
-"""Package __init__.py.
+"""Package __init__.py."""
 
-Taken from numpy that they use to import openblas.
-
-Helper to preload windows DLLs to prevent DLL not found errors.
-Once a DLL is preloaded, its namespace is made available to any
-subsequent DLL.
-"""
-
-import glob
 import os
-import sysconfig
 
 if os.name == "nt":
-    from ctypes import WinDLL
+    import sysconfig
 
-    basedir = sysconfig.get_path("purelib")
-    libs_dir = [os.path.abspath(os.path.join(basedir, "_wdm_lib", ".libs"))]
-    libs_dir += [os.path.abspath(os.path.join(basedir, "wdmtoolbox", ".libs"))]
-
-    for lib in libs_dir:
-        for filename in glob.glob(os.path.join(lib, "*.dll")):
-            if os.path.exists(filename):
-                _ = WinDLL(os.path.abspath(filename))
+    os.add_dll_directory(sysconfig.get_paths()["purelib"])
 
 __all__ = [
     "cleancopywdm",
